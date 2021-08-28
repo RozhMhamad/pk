5ce7f574c9cc23a85209d47712e659b1b8c008a2
MAPXOR12 <84636715+MAPXOR12@users.noreply.github.com>
2021-08-28 09:30:24 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const db = require('quick.db');
 const { TOKEN, PREFIX, AVATARURL, BOTNAME, } = require(`./config.json`);
 const figlet = require("figlet");
 const client = new Client({ disableMentions: `` , partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
-client.login('Nzg0MzA0ODQzODA3MzkxNzU1.X8nWzg.X9lo1NAjeP-4AsCBWi1p6BroZN0');
+client.login('Nzg0MzAzNzU2OTI1NDY4NzEz.X8nVyw.81oRigtb863GckiEAUD54Jce7Xw');
 client.commands = new Collection();
 client.setMaxListeners(0);
 client.prefix = PREFIX;
